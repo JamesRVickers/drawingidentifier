78f07477eb0c1eacb18c8f4479c5404c12472da5
James Vickers <vickerjame@myvuw.ac.nz>
2026-09-12 13:43:48 +1200

    Setup

---
+++ src/main/java/com/github/jamesrvickers/drawingidentifier/Main.java
@@ -0,0 +1,11 @@
+package main.java.com.github.jamesrvickers.drawingidentifier;
+
+import java.io.*;
+import javax.swing.*;
+
+public class Main {
+    JFrame frame = new JFrame();
+    frame.setSize(500, 600);
+    frame.setLayout(null);
+    frame.setVisible(true);
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module com.github.jamesrvickers.drawingidentifier {
+    exports com.github.jamesrvickers.drawingidentifier;
+}
